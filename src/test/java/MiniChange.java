369dd7cefffc3603a770477cd54ab3b638d2b13c
”dgdeshmukh” <dgdeshmukh09@gmail.com>
2025-09-15 20:06:26 +0530

    Add Variable_Constant.java

---
+++ src/test/java/MiniChange.java
@@ -0,0 +1,18 @@
+public class MiniChange 
+{
+
+    public static void main(String []args)
+    {
+        int maths=75;
+        int science=60;
+        int english=80;
+
+        int total=maths+science+english;
+
+        float percentage=total/3;
+
+        String result=(percentage>=40) ? "Pass" : "Fail";
+
+        System.out.println("Reult is : "+result);
+    }
+}
