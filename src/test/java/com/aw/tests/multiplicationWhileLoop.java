8c486dcbbb70b2329fa37e0bc92ac0a188b9d6d7
”dgdeshmukh” <dgdeshmukh09@gmail.com>
2025-09-16 12:50:32 +0530

    Add today's files

---
+++ src/test/java/com/aw/tests/multiplicationWhileLoop.java
@@ -0,0 +1,20 @@
+package com.aw.tests;
+
+public class multiplicationWhileLoop 
+{
+
+    public static void main(String[] args) 
+    {
+
+        int a=5 ;
+        int b=1;
+
+        while (b<=10)
+        {
+            System.out.println(a + "*" +b + "="+(a*b));
+
+            b++;
+        }
+    }
+
+}
