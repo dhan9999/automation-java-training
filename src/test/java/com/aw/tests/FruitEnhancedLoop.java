cfc7c501173df85acf2259079d2f5cc0a76a1de9
”dgdeshmukh” <dgdeshmukh09@gmail.com>
2025-09-16 12:45:14 +0530

    Add today's files

---
+++ src/test/java/com/aw/tests/FruitEnhancedLoop.java
@@ -0,0 +1,16 @@
+package com.aw.tests;
+
+public class FruitEnhancedLoop {
+
+    public static void main(String[] args) 
+    {
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange", "Grapes"};
+
+        System.out.println("List of Fruits:");
+
+        for (String fruit : fruits) 
+        {
+            System.out.println(fruit);
+    }
+}
+} 
